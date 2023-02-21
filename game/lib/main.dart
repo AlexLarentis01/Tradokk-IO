824b1f77d61c60f92588669ccc8d7239f18b3534
Alex Larentis <alex.larentis@tradokk.com>
2023-02-21 01:34:13 +0100

    adding lobby messages in frontend

--- game/lib/main.dart
+++ game/lib/main.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:game/ui/pages/home.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'src/app.dart';
+import 'src/settings/settings_controller.dart';
+import 'src/settings/settings_service.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() async {
+  // Set up the SettingsController, which will glue user settings to multiple
+  // Flutter Widgets.
+  final settingsController = SettingsController(SettingsService());
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Home(),
-    );
-  }
+  // Load the user's preferred theme while the splash screen is displayed.
+  // This prevents a sudden theme change when the app is first displayed.
+  await settingsController.loadSettings();
+
+  // Run the app and pass in the SettingsController. The app listens to the
+  // SettingsController for changes, then passes it further down to the
+  // SettingsView.
+  runApp(MyApp(settingsController: settingsController));
 }
--- game/test/widget_test.dart
+++ game/test/widget_test.dart
@@ -1,30 +1,79 @@
-// This is a basic Flutter widget test.
+// This is an example Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
+//
+// Visit https://flutter.dev/docs/cookbook/testing/widget/introduction for
+// more information about Widget testing.
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:game/src/lobby/lobby_service.dart';
+import 'package:mockito/mockito.dart';
 
-import 'package:game/main.dart';
+
+class MockLobbyService extends Mock implements LobbyService {
+  @override
+  bool connected = true;
+
+  
+  @override
+  void connectToLobby() {
+    // TODO: implement connectToLobby
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  // TODO: implement games
+  List get games => throw UnimplementedError();
+
+  @override
+  // TODO: implement hasListeners
+  bool get hasListeners => throw UnimplementedError();
+
+  @override
+  Stream<String> listen() {
+    // TODO: implement listen
+    throw UnimplementedError();
+  }
+
+  @override
+  void notifyListeners() {
+    // TODO: implement notifyListeners
+  }
+
+  @override
+  void removeListener(VoidCallback listener) {
+    // TODO: implement removeListener
+  }
+
+  @override
+  void send(message) {
+    // TODO: implement send
+  }
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('MyWidget', () {
+    testWidgets('should display a string of text', (WidgetTester tester) async {
+      // Define a Widget
+      const myWidget = MaterialApp(
+        home: Scaffold(
+          body: Text('Hello'),
+        ),
+      );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      // Build myWidget and trigger a frame.
+      await tester.pumpWidget(myWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Verify myWidget shows some text
+      expect(find.byType(Text), findsOneWidget);
+    });
   });
 }
